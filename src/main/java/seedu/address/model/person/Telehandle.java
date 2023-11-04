5d00d2aa6e21f8912e62abcdcc5791750f5ad222
leontan2 <leontan2638@gmail.com>
2023-11-04 20:14:51 +0800

    Include error description for telehandles consisting of only numbers

--- src/main/java/seedu/address/model/person/Telehandle.java
+++ src/main/java/seedu/address/model/person/Telehandle.java
@@ -10,7 +10,8 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Telehandle {
 
     public static final String MESSAGE_CONSTRAINTS = "Telehandles must start with '@', cannot contain spaces, and "
-            + "should consist of letters, numbers, and underscores only. No other special characters are allowed.";
+            + "should consist of letters, numbers, and underscores only. No other special characters are allowed. "
+            + "Additionally, telehandles cannot consist of only numbers."
 
     /*
      * The first character of the telehandle must be a @,
